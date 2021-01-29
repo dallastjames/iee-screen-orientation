66ab95f0c663fb301d81d5cfe7be9091e31f13e0
Dallas James <dallas@Dallass-MacBook-Pro.local>
2021-01-29 13:04:16 -0700

    init to win  it

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ScreenOrientation } from "@ionic-enterprise/screen-orientation/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  constructor(private screen: ScreenOrientation) {}
 
-  constructor() {}
+  public lock() {
+    this.screen.lock("portrait");
+  }
 
+  public unlock() {
+    this.screen.unlock();
+  }
 }
